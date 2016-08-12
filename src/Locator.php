b6ef892f9982c9a8ba17a9c17e0a2f581a8f346e
Krzysztof Kurzydlo <krzysztof.kurzydlo@creativestyle.pl>
2016-08-12 10:52:12 +0200

    Added interface for locator. Changed old Locator to TemplateLocator

--- src/Locator.php
+++ src/Locator.php
@@ -2,15 +2,13 @@
 
 namespace Creativestyle\CreativePatterns;
 
-class Locator
+interface Locator
 {
-    private static function getBasePath() {
-        return realpath(__DIR__.'/../');
-    }
-
-    public static function locateTemplate($componentName, $templateName) {
-        $path = self::getBasePath().'/packages/components/'.$componentName.'/src/'.$templateName;
-
-        return $path;
-    }
+    /**
+     * Returns realpath for searched file
+     * @param $module
+     * @param $file
+     * @return string
+     */
+    public function locate($component, $file);
 }
