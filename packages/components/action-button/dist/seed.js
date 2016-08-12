b746cf6ca5835aa0b2060632660b0b18b67298a6
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2016-08-12 10:47:09 +0200

    Adding action button component.

---
+++ packages/components/action-button/dist/seed.js
@@ -0,0 +1,12 @@
+(function (exports) {
+    'use strict';
+
+    function seed(a, b) {
+        return a + b;
+    }
+    ;
+
+    exports.seed = seed;
+
+}((this.seed = this.seed || {})));
+//# sourceMappingURL=seed.js.map
