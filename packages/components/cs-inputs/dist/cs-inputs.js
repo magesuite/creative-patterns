9ddd5430aa47a90bed0295de49e384cae160a24b
Maciej Borkowski <maciej.borkowski@creativestyle.pl>
2016-08-30 10:13:01 +0200

    [cs-inputs] Alpha - only what is needed to build headline configurator component

---
+++ packages/components/cs-inputs/dist/cs-inputs.js
@@ -0,0 +1,12 @@
+(function (exports) {
+    'use strict';
+
+    function add(a, b) {
+        return a + b;
+    }
+    ;
+
+    exports.add = add;
+
+}((this.csInputs = this.csInputs || {})));
+//# sourceMappingURL=cs-inputs.js.map
