acdd8a89c6b644a3084a823050c629b103f0ba78
Maciej Borkowski <maciej.borkowski@creativestyle.pl>
2016-09-05 10:57:01 +0200

    [cs-input] Renamed component, created m2 backend skin

---
+++ packages/components/cs-input/src/tests/unit/cs-input.ts
@@ -0,0 +1,7 @@
+import { add } from '../../cs-input';
+
+describe( 'A suite is just a function', function(): void {
+  it( 'and so is a spec', function(): void {
+    expect( add( 1, 2 ) ).toBe( 3 );
+  } );
+} );
---
+++ packages/customizations/m2-input/dist/m2-input.js
@@ -0,0 +1,12 @@
+(function (exports) {
+    'use strict';
+
+    function add(a, b) {
+        return a + b;
+    }
+    ;
+
+    exports.add = add;
+
+}((this.m2Input = this.m2Input || {})));
+//# sourceMappingURL=m2-input.js.map
