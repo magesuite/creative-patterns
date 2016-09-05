b834b0254701d0adb053649aa3bfc2c6e0dcc6a1
Lukasz Ostrowski <lukasz.ostrowski@creativestyle.pl>
2016-09-05 10:00:58 +0200

    product tile component

--- packages/components/cs-image-teaser/dist/cs-image-teaser.js
+++ packages/components/cs-image-teaser/dist/cs-image-teaser.js
@@ -1,12 +1,17 @@
-(function (exports) {
-    'use strict';
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+    typeof define === 'function' && define.amd ? define('csImageTeaser', ['exports'], factory) :
+    (factory((global.csImageTeaser = global.csImageTeaser || {})));
+}(this, (function (exports) { 'use strict';
 
-    function add(a, b) {
-        return a + b;
-    }
-    ;
+function add(a, b) {
+    return a + b;
+}
+;
 
-    exports.add = add;
+exports.add = add;
 
-}((this.csImageTeaser = this.csImageTeaser || {})));
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
 //# sourceMappingURL=cs-image-teaser.js.map
