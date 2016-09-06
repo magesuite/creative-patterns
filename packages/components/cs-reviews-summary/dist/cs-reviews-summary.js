6cab4836b2577a823b6104bdf5d4bc0bdd8413fb
Lukasz Ostrowski <lukasz.ostrowski@creativestyle.pl>
2016-09-06 09:37:56 +0200

    reviews summary component without progress bar

---
+++ packages/components/cs-reviews-summary/dist/cs-reviews-summary.js
@@ -0,0 +1,12 @@
+(function (exports) {
+    'use strict';
+
+    function add(a, b) {
+        return a + b;
+    }
+    ;
+
+    exports.add = add;
+
+}((this.csReviewsSummary = this.csReviewsSummary || {})));
+//# sourceMappingURL=cs-reviews-summary.js.map
