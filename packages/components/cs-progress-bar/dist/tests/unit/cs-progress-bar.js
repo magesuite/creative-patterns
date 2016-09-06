e6f055c30eb9a3839fd4cf959c149349f346f1a8
Lukasz Ostrowski <lukasz.ostrowski@creativestyle.pl>
2016-09-06 11:22:35 +0200

    reviews summary with progress bar

--- packages/components/cs-progress-bar/dist/tests/unit/cs-progress-bar.js
+++ packages/components/cs-progress-bar/dist/tests/unit/cs-progress-bar.js
@@ -1,16 +1,7 @@
-(function (exports) {
-    'use strict';
+(function () {
+	'use strict';
 
-    function add(a, b) {
-        return a + b;
-    }
-    ;
 
-    describe('A suite is just a function', function () {
-        it('and so is a spec', function () {
-            expect(add(1, 2)).toBe(3);
-        });
-    });
 
-}((this.csProgressBar = this.csProgressBar || {})));
+}());
 //# sourceMappingURL=cs-progress-bar.js.map
