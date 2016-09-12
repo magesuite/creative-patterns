47f8894b8a6ea4f9eeb30d3bf56d52aa39d95a29
Lukasz Ostrowski <lukasz.ostrowski@creativestyle.pl>
2016-09-12 10:48:34 +0200

    image teaser component

---
+++ packages/components/cs-image-teaser/dist/cs-image-teaser.js
@@ -0,0 +1,12 @@
+(function (exports) {
+    'use strict';
+
+    function add(a, b) {
+        return a + b;
+    }
+    ;
+
+    exports.add = add;
+
+}((this.csImageTeaser = this.csImageTeaser || {})));
+//# sourceMappingURL=cs-image-teaser.js.map
