8dd471daf929f56def6170a05bf373df4b7dd7b1
Lukasz Ostrowski <lukasz.ostrowski@creativestyle.pl>
2016-09-16 13:40:19 +0200

    added single review component

---
+++ packages/components/cs-single-review/dist/cs-single-review.js
@@ -0,0 +1,17 @@
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+    typeof define === 'function' && define.amd ? define('csSingleReview', ['exports'], factory) :
+    (factory((global.csSingleReview = global.csSingleReview || {})));
+}(this, (function (exports) { 'use strict';
+
+function add(a, b) {
+    return a + b;
+}
+;
+
+exports.add = add;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
+//# sourceMappingURL=cs-single-review.js.map
