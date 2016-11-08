3c2ffab59a633b497e4cb50727619e167bc27fa8
Maciej Borkowski <maciej.borkowski@creativestyle.pl>
2016-11-08 09:09:08 +0100

    [feature-cs-grid-product][feature-cs-items-grid]

---
+++ packages/components/cs-grid-product/dist/tests/unit/cs-grid-product.js
@@ -0,0 +1,9 @@
+(function (exports) {
+'use strict';
+
+describe('cs-grid-product', function () {
+    xit('should have tests either implemented or removed if it has no scripts.');
+});
+
+}((this.csGridProduct = this.csGridProduct || {})));
+//# sourceMappingURL=cs-grid-product.js.map
---
+++ packages/components/cs-items-grid/dist/cs-items-grid.js
@@ -0,0 +1,10 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+	typeof define === 'function' && define.amd ? define('csItemsGrid', factory) :
+	(factory());
+}(this, (function () { 'use strict';
+
+
+
+})));
+//# sourceMappingURL=cs-items-grid.js.map
