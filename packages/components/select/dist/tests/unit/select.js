e8204439eecf9ba15d0ead5b9a0e2cdfd86407be
Maciej Borkowski <maciej.borkowski@creativestyle.pl>
2016-11-29 11:57:11 +0100

    [select] select component with fallback to native select for mobiles

---
+++ packages/components/select/dist/tests/unit/select.js
@@ -0,0 +1,9 @@
+(function (exports) {
+'use strict';
+
+describe('select', function () {
+    xit('should have tests either implemented or removed if it has no scripts.');
+});
+
+}((this.select = this.select || {})));
+//# sourceMappingURL=select.js.map
---
+++ packages/components/select/src/tests/unit/select.ts
@@ -0,0 +1,3 @@
+describe( 'select', function(): void {
+    xit( 'should have tests either implemented or removed if it has no scripts.');
+} );
